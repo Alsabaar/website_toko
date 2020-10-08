cfb6a0e20bf19d0c2341d43ddce9fe9e7f3c8949
al amin sabaar <45156470+Alsabaar@users.noreply.github.com>
2020-10-08 22:20:52 +0900

    Add files via upload

---
+++ AlAminSyahrudinSabaarTug185410009/about.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Toko elektronik </title>
+    <link rel="stylesheet" href="css/tampilan.css">
+
+</head>
+
+<body>
+
+    <div class="badan-utama">
+
+        <header>
+            <h2>Toko Elektronik Sabaar</h2>
+            <p>Toko Online untuk kita semua</p>
+        </header>
+
+        <nav class="navigasi">
+            <ul>
+                <li><a class="active" href="beranda.php">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">explore</a></li>
+                <li><a href="store.php">lainnya</a></li>
+
+            </ul>
+        </nav>
+
+        <div class="menu-tengah">
+            <div class="kotak">
+
+                <span class="tanggal-posting">
+                    Diposting pada 10.30 WIT, 08 oktober 2020
+                </span>
+
+                <img src="image/logo.png" alt=" foto">
+
+                <p>Nama : Al Amin Syahrusin Sabaar</p>
+                <p>Nim : 185410009</p>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        @copyright 2020 || by alamin syahrudin sabaar
+    </footer>
+
+    </div>
+
+</body>
+
+</html
---
+++ AlAminSyahrudinSabaarTug185410009/beranda.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Toko elektronik </title>
+    <link rel="stylesheet" href="css/tampilan.css">
+
+</head>
+
+<body>
+    <div class="badan-utama">
+
+        <header>
+            <h2>Toko Elektronik Sabaar</h2>
+            <p>Toko Online untuk kita semua</p>
+        </header>
+
+        <nav class="navigasi">
+            <ul>
+                <li><a class="active" href="#home">Home</a></li>
+                <li><a href="menu.php">Menu</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">explore</a></li>
+                <li><a href="store.php">lainnya</a></li>
+            </ul>
+        </nav>
+
+        <div class="banner">
+
+        </div>
+
+        <div class="menu-kiri">
+            <div class="kotak">
+                <h3>Pemesanan</h3>
+                <p>
+                    Isi Form Pemesanan dengan Klik Tombol di bawah ini
+                </p>
+
+                <a class="tombol tombol-pesan" href="#">Pesan</a>
+            </div>
+            <div class="kotak">
+                <h3>Pemesanan</h3>
+                <p>
+                    Isi Form Pemesanan dengan Klik Tombol di bawah ini
+                </p>
+
+                <a class="tombol tombol-pesan" href="#">Pesan</a>
+            </div>
+            <div class="kotak">
+                <h3>Pemesanan</h3>
+                <p>
+                    Isi Form Pemesanan dengan Klik Tombol di bawah ini
+                </p>
+
+                <a class="tombol tombol-pesan" href="#">Pesan</a>
+            </div>
+        </div>
+
+        <div class="menu-tengah">
+            <div class="kotak">
+                <h3> ASUS VivoBook 4K Laptop Specs & Price</h3>
+                <span class="tanggal-posting">
+                    Diposting pada 10.30 WIT, 08 oktober 2020
+                </span>
+
+                <img src="image/asus2.png" alt=" laptop aus">
+
+                <p>
+                    VivoBook 4K adalah laptop Windows 10 baru yang menakjubkan dari ASUS yang dirancang untuk tampilan
+                    yang mengagumkan dan kinerja yang mengesankan. Menampilkan layar 4K yang brilian, laptop bergaya ini
+                    dilengkapi untuk kinerja luar biasa dengan prosesor Intel Core i7 generasi terbaru, kapasitas RAM
+                    yang sangat besar, dan grafis diskrit NVIDIA
+                </p>
+
+                <a class="tombol tombol-lengkap" href="asus.php">Selengkapnya</a>
+
+                <h3>Camera EOS 5D Mark IV </h3>
+
+
+                <img src="image/kamera.png" alt=" kamera">
+
+                <p>
+                    Sebagai generasi ke-4 dari kamera EOS full-frame Canon paling laris, EOS 5D Mark IV semakin matang
+                    dan m
+                    emiliki peningkatan besar yang layak dibanggakan. Sensor CMOS full-frame 30.4 megapiksel yang
+                    dilengkapi dengan Dual Pixel CMOS AF. AF berkecepatan tinggi dan memiliki presisi tinggi yang
+                    bukan hanya untuk memotret tetapi juga untuk pembuatan film 4K yang memungkinkan pembuatan film
+                    profesional. Dengan fleksibilitas yang dimilikinya, apa pun subjeknya, DSLR canggih ini mampu
+                    menuangkan gagasan kreatif para fotografer profesional menjadi karya yang mengagumkan.
+                </p>
+
+                <a class="tombol tombol-lengkap" href="camera.php">Selengkapnya</a>
+
+            </div>
+        </div>
+
+        <div class="menu-kanan">
+            <div class="kotak">
+                <img src="image/windows.jpg" alt="windows">
+                <left>
+                    <p>Windows 10 adalah versi hebat dari sistem operasi Microsoft, namun beberapa masalah perlu diubah
+                        dan peningkatan dapat membuat beberapa masalah kecil bagi pengguna. Kami telah melakukan
+                        penginstalan / peningkatan yang tak terhitung jumlahnya dan dapat menyelesaikan masalah sisa
+                        yang mungkin Anda miliki. Hubungi kami dan jalankan Windows Anda sebagaimana mestinya</p>
+                </left>
+            </div>
+            <div class="kotak">
+                <h3>Berita Terbaru</h3>
+                <nav class="menu-artikel">
+                    <ul>
+                        <li><a href="#">ASUS LAPTOP REPAIR ABMON</a></li>
+                        <li><a href="#">IT SUPPORT</a></li>
+                        <li><a href="#">INTERNET & WIFI-SERVICE</a></li>
+                        <li><a href="#">SMALL BUSSINES IT SUPPORT</a></li>
+                        <li><a href="#">MALWARE & VIRUS REMOVAL</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <footer>
+            @copyright 2020 || by alamin syahrudin sabaar
+        </footer>
+
+    </div>
+
+</body>
+
+</html
